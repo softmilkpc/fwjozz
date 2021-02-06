8a53e54672185f0e7fd4e1b933986bacbe310fb5
softmilkpc <70849324+softmilkpc@users.noreply.github.com>
2021-02-06 16:17:35 +0700

    Create config.py

---
+++ forwarder/config.py
@@ -0,0 +1,35 @@
+if not __name__.endswith("sample_config"):
+    import sys
+    print("The README is there to be read. Extend this sample config to a config file, don't just rename and change "
+          "values here. Doing that WILL backfire on you.\nBot quitting.", file=sys.stderr)
+    quit(1)
+
+
+# Create a new config.py file in same dir and import, then extend this class.
+class Config(object):
+    LOGGER = True
+
+    # REQUIRED
+    API_KEY = "1604114888:AAFBMW3WKhjniz23BQONIRo-u2EFiWff69I"  # API key obtained from BotFather
+    OWNER_ID = "912557966"  # If you dont know, run the bot and do /id in your private chat with the bot
+
+    # FOR AUTOMATICALLY FORWARDING MESSAGES
+    FROM_CHATS = [-479039979 ]  # List of chat id's to forward messages from
+    TO_CHATS = [-1257954112, -1413336567, -405519826, -1452921309, -1409061137]  # List of chat id's to forward messages to
+
+    # FOR WEBHOOKS
+    WEBHOOK = False
+    IP_ADDRESS = "127.0.0.1"  # Use "0.0.0.0" if using Heroku
+    URL = None  # The URL that the bot should listen to for updates
+    PORT = 5000  # Port to listen on for webhooks
+    CERT_PATH = None  # Path to certificate file
+
+    WORKERS = 4  # Depends on usage, 4 by default
+
+
+class Production(Config):
+    LOGGER = False
+
+
+class Development(Config):
+    LOGGER = True
